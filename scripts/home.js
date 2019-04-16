855ee73c295e65ec56ad3841e1087be61e76539e
Cathy <cathy.dolle@live.fr>
2019-04-16 11:55:33 +0200

    HOME
    :sparkles:

---
+++ scripts/home.js
@@ -0,0 +1,35 @@
+(function($){
+
+	/* ---------------------------------------------- /*
+	 * Preloader
+	/* ---------------------------------------------- */
+
+	$(window).load(function() {
+		$('#status').fadeOut();
+		$('#preloader').delay(350).fadeOut('slow');
+	});
+
+	$(document).ready(function() {
+
+		/* ---------------------------------------------- /*
+		 * Animated scrolling / Scroll Up
+		/* ---------------------------------------------- */
+
+		$('a[href*=#]').bind("click", function(e){
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top
+			}, 1000);
+			e.preventDefault();
+		});
+
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 100) {
+				$('.scroll-up').fadeIn();
+			} else {
+				$('.scroll-up').fadeOut();
+			}
+		});
+	});
+
+})(jQuery);
