5592dc04d14830c6f0c442e39245cb921029d763
Maxime Druart <maxime.druart@yahoo.fr>
2019-04-18 11:20:58 +0200

    déplacement du ninja sur l'axe Z + item fix sur Z

--- scripts/mainClone.js
+++ scripts/mainClone.js
@@ -30,6 +30,7 @@ class GameLevel {
     //   }
     // } // dans le flou pour l'instant, on veut faire apparaitre les cases les unes après les autres mais il y a des spicy problèmes avec this dans le setTimeout
   }
+
   levelInit() {
     this.context.clearRect(-this.width / 2, -200, this.width, this.height)
     this.drawMap()
@@ -39,14 +40,14 @@ class GameLevel {
     this.context.clearRect(-this.width / 2, -200, this.width, this.height)
   }
 
-  drawMapItem(x, y, item) {
+  drawMapItem(x, y, z, item) {
     let image = document.createElement("img")
     image.src = "images/assets/" + item + ".svg"
     image.transform = "scaleX(-1)"
     let self = this
     image.onload = function() {
       self.context.save()
-      self.context.translate((x - y) * tileWidth / 2, (x + y) * self.tileHeight / 2)
+      self.context.translate((x - y) * tileWidth / 2, ((x + y) * self.tileHeight / 2) - z * self.tileHeight)
       self.context.drawImage(image, -image.width / 2, -image.height) // A ECRIRE
       self.context.restore()
     }
@@ -118,8 +119,8 @@ class GameLevel {
     for (let i = 0; i < this.map.length; i++) {
       for (let j = 0; j < this.map[i].length; j++) {
         if (this.map[i][j][0] == 10) {
-          this.spawn = [i, j]
-        }
+          this.spawn = [i, j, this.map[i][j][1]]
+        } // on note le spawn pour le ninja
         if (this.map[i][j][0] != 0 && this.map[i][j][0] != 8) {
           this.drawBlock(i, j, this.map[i][j][1], this.map[i][j][2], "grey") // case basique
         } else if (this.map[i][j][0] == 8) {
@@ -130,14 +131,14 @@ class GameLevel {
           if (itemToDraw == 6) { // les téléporteurs entrée / sortie ont des codes différents mais la meme image
             itemToDraw = 5
           }
-          this.drawMapItem(i + 0.25, j + 0.25, itemToDraw)
+          this.drawMapItem(i + 0.8, j + 0.8, this.map[i][j][1], itemToDraw) // on envoie x, y, zStart et l'item code
         }
       }
     }
 
     if (reset) {
       if (ninja == 0) { // on créé un ninja pour le premier niveau
-        ninja = new Character(this.spawn[0], this.spawn[1], this.level)
+        ninja = new Character(this.spawn[0], this.spawn[1], this.spawn[2], this.level)
         ninja.startImage.onload = function() { // obligé de le faire en dehors de l'objet ou sinon c'est des conflits entre les this.
           ninja.eventStart() // dessin initial
           ninja.moveEL() // on lance l'event listener de déplacement
@@ -146,9 +147,10 @@ class GameLevel {
       } else { // pour les autres niveaux, on update son x, y, level, map et le redraw
         ninja.x = this.spawn[0]
         ninja.y = this.spawn[1]
+        ninja.z = this.spawn[2]
         ninja.level++
         ninja.charModMap = maps[this.level]
-        ninja.drawCharacter(ninja.startImage, this.spawn[0], this.spawn[1])
+        ninja.drawCharacter(ninja.startImage, this.spawn[0], this.spawn[1] ,this.spawn[2])
         ninja.startImage.onload = function() {
           ninja.eventStart() // dessin initial
           ninja.moveEL() // on lance l'event listener de déplacement
@@ -166,11 +168,12 @@ class GameLevel {
 
 
 class Character {
-  constructor(x, y, level) {
+  constructor(x, y, z, level) {
     this.level = level
     this.charModMap = maps[level] // on garde une copie dans le perso de la map actuelle, on veut pas modifier la map sur maps.js a chaque fois
     this.x = x
     this.y = y
+    this.z = z
     this.canvas = document.querySelector("#charCanvas")
     this.ctx = this.canvas.getContext("2d")
     this.cWidth = this.canvas.width = window.innerWidth / 1.7 // a modifier pour la taille
@@ -204,7 +207,8 @@ class Character {
                 self.charModMap[xyRed[0]][xyRed[1]][0] = 1
                 levels[self.level].drawMap(self.charModMap, false)
               }
-              self.drawCharacter(self.finalImages[2], self.x, self.y)
+              self.z = self.zAdjusting(self.x, self.y, self.level)
+              self.drawCharacter(self.finalImages[2], self.x, self.y, self.z)
 
             }
             break;
@@ -220,7 +224,8 @@ class Character {
                 self.charModMap[xyRed[0]][xyRed[1]][0] = 1
                 levels[self.level].drawMap(self.charModMap, false)
               }
-              self.drawCharacter(self.finalImages[0], self.x, self.y)
+              self.z = self.zAdjusting(self.x, self.y, self.level)
+              self.drawCharacter(self.finalImages[0], self.x, self.y, self.z)
             }
             break;
           case 39: // RIGHT
@@ -235,7 +240,8 @@ class Character {
                 self.charModMap[xyRed[0]][xyRed[1]][0] = 1
                 levels[self.level].drawMap(self.charModMap, false)
               }
-              self.drawCharacter(self.finalImages[3], self.x, self.y)
+              self.z = self.zAdjusting(self.x, self.y, self.level)
+              self.drawCharacter(self.finalImages[3], self.x, self.y, self.z)
             }
             break;
           case 40: // DOWN
@@ -250,7 +256,8 @@ class Character {
                 self.charModMap[xyRed[0]][xyRed[1]][0] = 1
                 levels[self.level].drawMap(self.charModMap, false)
               }
-              self.drawCharacter(self.finalImages[1], self.x, self.y)
+              self.z = self.zAdjusting(self.x, self.y, self.level)
+              self.drawCharacter(self.finalImages[1], self.x, self.y, self.z)
             }
             break;
         }
@@ -263,7 +270,7 @@ class Character {
     this.ctx.clearRect(-this.cWidth / 2, -200, this.cWidth, this.cHeight)
   }
 
-  floorTest(x, y, level) {
+  floorTest(x, y, level) { // on teste les boutons, les tp et l'arrivée
     if (maps[level][x][y][0] == 100 || maps[level][x][y][0] == 1000 || maps[level][x][y][0] == 10000) { // on est donc sur l'arrivée
       levelsCompleted++ // obsolète (je crois)
       nextLevel()
@@ -307,16 +314,20 @@ class Character {
     }
   }
 
-  drawCharacter(image, x, y) {
+  drawCharacter(image, x, y, z) {
     this.ctx.clearRect(-this.cWidth / 2, -200, this.cWidth, this.cHeight)
     this.ctx.save()
-    this.ctx.translate((x - y) * tileWidth / 2, (x + y) * tileHeight / 2 + 13); // on se déplace a l'endroit de la case d'après
+    this.ctx.translate((x - y) * tileWidth / 2, ((x + y) * tileHeight / 2 + 35) - z *tileHeight); // on se déplace a l'endroit de la case d'après. ajustements de 35 pour le faire descendre un peu
     this.ctx.drawImage(image, -this.startImage.width / 2, -this.startImage.height)
     this.ctx.restore()
   }
 
   eventStart() {
-    this.drawCharacter(this.startImage, this.x, this.y)
+    this.drawCharacter(this.startImage, this.x, this.y, this.z)
+  }
+
+  zAdjusting(x, y, level){
+    return maps[level][x][y][1]
   }
 
   canMove(x, y, level) { // on doit appeler sinon il y a un problème de scope je sais pas pourquoi
@@ -417,7 +428,7 @@ class Character {
       }
     })
 
-    this.drawCharacter(this.activeImage, this.x, this.y) // on redessine le personnage au cas ou il a sauté
+    this.drawCharacter(this.activeImage, this.x, this.y, this.z) // on redessine le personnage au cas ou il a sauté
     levels[this.level].drawMap(this.charModMap, false) // on redraw la map au cas ou elle a été modifié
   }
 
